10a7b4c0c1ad578e6e19af0401d4b45fbd0a92f6
Tom Large <tomlarge@dhcp-10-105-146-210.wireless.northwestern.private>
2017-02-27 00:14:57 -0600

    novelty curve - testing still needed

--- segment.py
+++ segment.py
@@ -1,24 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
+#import matplotlib
 import sklearn
-import librosa
+from librosa import load,stft,logamplitude
 import cmath
 import math
 import scipy
 import random
 
-#import audio
-music, sr = librosa.load('./call_me_maybe.mp3')
+#initialize globals
 hop_length = 1024
 n_fft = 2048
 
+def load_music(path):
+    """
+    Returns: loaded music and sample rate given a string of a path to the audio file
+    path: string
+    """
+    music, sr = load(path)
+    return music,sr
 
-stft = librosa.stft(music, hop_length = hop_length, n_fft = n_fft)
-log_spectrogram = librosa.logamplitude(np.abs(stft**2), ref_power=np.max)
+def feature_vectors(music,hop_length,window_size):
+    """
+    Returns: feature vectors for audio sample music from the log spectrogram of the music
+    music: 1-D numpy array
+    hop_length: int
+    window_size: int
+    """
+    _stft = stft(music, hop_length = hop_length, n_fft = n_fft)
+    log_spectrogram = logamplitude(np.abs(_stft**2), ref_power=np.max)
+    return log_spectrogram
 
-feature_vectors = log_spectrogram #for simplified naming
-def sim_matrix(feature_vectors, sample_rate, hop_length, distance_metric = 'cosine', display = True):
+def sim_matrix(feature_vectors, sample_rate, hop_length, distance_metric = 'cosine',display=True):
     """
         Input:
             feature_vectors - a numpy ndarray MxN, where M is the number of features in each vector and
@@ -33,8 +46,7 @@ def sim_matrix(feature_vectors, sample_rate, hop_length, distance_metric = 'cosi
             returns sim_matrix - an NxN matrix with the pairwise distance between every feature vector.
     """
     dist_mat = scipy.spatial.distance.squareform(scipy.spatial.distance.pdist(feature_vectors.T,distance_metric))
-    sim_mat = 1 - dist_mat/np.max(dist_mat)
-
+    sim_mat = 1 - dist_mat/dist_mat.max()
     if display:
         plt.figure()
         plt.imshow(sim_mat)
@@ -47,13 +59,51 @@ def sim_matrix(feature_vectors, sample_rate, hop_length, distance_metric = 'cosi
         plt.xlabel('Time (s)')
         plt.ylabel('Time (s)')
         plt.title('Similarity matrix')
-        plt.show()
     return sim_mat
 
 def gaussian_checkerboard_kernel(M):
-    """ Creates gaussian checkerboard kernel of size M (even)"""
+    """
+    Returns: 2-D gaussian checkerboard kernel of size M (even)
+    M: int
+    """
     g = scipy.signal.gaussian(M, M / 3., sym=True)
     G = np.dot(g.reshape((-1, 1)), g.reshape((1, -1)))
     G[M / 2:, :M / 2] = -G[M / 2:, :M / 2]
-    #G[:M / 2, M / 2:] = -G[:M / 2, M / 2:]
+    G[:M / 2, M / 2:] = -G[:M / 2, M / 2:]
     return G
+
+def novelty_curve(sim_mat,ker_size):
+    """
+    Returns: 1-D numpy array of the novelty curve of similarity matrix sim_mat and kernel size ker_size
+    sim_mat: 2-D numpy array of similarity matrix
+    ker_size: int of kernel size
+    """
+    #compute similarity matrix in lag domain
+    # r = np.floor(ker_size/2)
+    # s_lag = np.zeros((sim_mat.shape[0],sim_mat.shape[1] + ker_size - r))
+    # for i in range(sim_mat.shape[0]):
+    #     for l in range(ker_size):
+    #         s_lag[i,l] = sim_mat[i,i+l-r]
+    novelty_curve = np.zeros(sim_mat.shape[0])
+    padded_sim_mat = np.pad(sim_mat,ker_size/2,'constant')
+
+    kernel = gaussian_checkerboard_kernel(ker_size)
+    i = 0
+    for r in range(ker_size/2,sim_mat.shape[0]+ker_size/2):
+        novelty_curve[r-ker_size/2] = np.sum(padded_sim_mat[r-ker_size/2:r+ker_size/2,r-ker_size/2:r+ker_size/2]*kernel)
+
+    #normalize
+    novelty_curve += novelty_curve.min()
+    novelty_curve = novelty_curve/novelty_curve.max()
+    return novelty_curve
+
+hop_length = 1024
+window_size = 2048
+music,sr = load_music("./call_me_maybe.mp3")
+feature_vectors = feature_vectors(music,hop_length,window_size)
+sim_mat = sim_matrix(feature_vectors,sr,hop_length,display=True)
+novelty_curve = novelty_curve(sim_mat,256)
+plt.figure()
+plt.imshow(sim_mat)
+plt.plot(novelty_curve)
+plt.show()
